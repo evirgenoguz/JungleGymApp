36b2da759470cc8f0d575f0704c8c35209d0cb6b
Oğuz Evirgen <evirgenoguz@gmail.com>
2023-11-12 01:33:00 +0300

    base :
    - url changed

--- app/src/main/java/com/evirgenoguz/junglegymapp/core/common/Constants.kt
+++ app/src/main/java/com/evirgenoguz/junglegymapp/core/common/Constants.kt
@@ -2,6 +2,6 @@ package com.evirgenoguz.junglegymapp.core.common
 
 object Constants {
 
-    const val BASE_URL = "www.sample-base-url/"
+    const val BASE_URL = "https://www.sample-base-url/"
 
 }
